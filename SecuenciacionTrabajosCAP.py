ef977192e8badd138c749d9f8813cf85dc0ca4fc
sofiaariza <79118546+sofiaariza@users.noreply.github.com>
2021-02-25 05:01:12 -0500

    Unión de la heurística: Best Fit Decreasing y el algoritmo: Scheduling to Minimize Lateness (Earliest Deadline First)
    Se realizó una corrección de la función agendamientoCamas que recibía las tareas de mayor a menor di.
    Se realizó una restricción para evitar que los trabajos que representan un mayor tiempo de procesamiento en comparación al plazo de entrega no sean aceptados.
    Se calculó el di y ti de cada cama para posteriormente unir la heurística: Best Fit Decreasing y el algoritmo: Scheduling to Minimize Lateness.

--- SecuenciacionTrabajosCAP.py
+++ SecuenciacionTrabajosCAP.py
@@ -2,11 +2,9 @@ INFINITO = float('Inf')
 def calcularAreaHorizontal(largo, ancho):
     area= largo*ancho
     return area
-
 def calcularVolumen(area, altura):
     volumen= area*altura
     return volumen
-
 def tiposPartes(nomParte):
     #Si se modifican las categorias, recordar modificar el numero al final
     print("Ingrese el numero que coincida con el tipo de la parte " + nomParte + "\n")
@@ -18,7 +16,6 @@ def tiposPartes(nomParte):
     print("5. Piezas delicadas (soportes para circuitos o pequeños)")
     cantidadTiposDePartes= 6
     return cantidadTiposDePartes
-
 def organizarPartesCategoria(cantidadTiposDePartes):
     global partes
     partesPorTipo= [[] for _ in range(cantidadTiposDePartes)]
@@ -29,33 +26,32 @@ def organizarPartesCategoria(cantidadTiposDePartes):
                 partesPorTipo[i].append(j)
     return partesPorTipo
 
-def agendamientoCamas(A=None,s= None):
-    if((not A) and (not s)):
-        A=[("C1",5,1),("C2",3,2),("C3",6,5),("C4", 1,1)]
-        s=7
-    A = sorted(A, key = lambda x: x[1], reverse=True)
-    ans,N = [],len(A)
+def agendamientoCamas(bins,s):
+    camasOrdenadas=sorted(bins.keys(), key = lambda x: bins[x][1])
+    ans,N = [],len(camasOrdenadas)
     f=s
-    for n in range(N):
+    for bin in camasOrdenadas:
         si=f
-        fi=f+A[n][2]
-        f=f+A[n][2]
-        ans.append([A[n][0], si, fi])
+        si=si%24
+        ti=bins[bin][2]
+        fi=f+ti
+        fi=fi%24
+        f=fi
+        ans.append([bin, bins[bin][0], si, fi])
     return ans
-#A=[... , ("Ci", di, ti), ...]
-A=[("C1",5,1),("C2",3,2),("C3",6,5),("C4", 1,1)]
-s=7
-#print(agendamientoCamas(A,s))
-#print(agendamientoCamas())
-
 def ocupadoBin(bin, partes):
     """
     Calcula cuan ocupado esta un bin en el momento actual
     """
     ocupado = 0
-    for parte in bin:
+    for parte in bin[0]:
         ocupado += partes[parte][8]
     return ocupado
+def encontrartiCama(bin, partes):
+    ti=0
+    for i in bin[0]:
+        ti+= partes[i][7]
+    return ti
 def bestFit(sizeBin, bins, partes, ordenados):
     for j in ordenados:
         flag=0
@@ -64,25 +60,27 @@ def bestFit(sizeBin, bins, partes, ordenados):
             ocupacion = ocupadoBin(bins[bin], partes)
             pesoTrabajo=partes[j][8]
             restante=sizeBin-ocupacion-pesoTrabajo
+            tiActualCama= encontrartiCama(bins[bin], partes)
+            tiParte= partes[j][7]
             if(restante>=0):
-                flag=1
                 #print("Si cabe", j, "pesa" ,pesoTrabajo, "en", bin + "." , "Hay de ocupacion", ocupacion, "y queda" , restante)
-                if(restante<restante2):
+                if((restante<restante2) and ((tiActualCama + tiParte)<= bins[bin][1]) and ((tiActualCama + tiParte)<= partes[j][6])):
+                    flag=1
                     restante2=restante
                     binSeleccionado= bin
                     #print("el bin seleccionado es", binSeleccionado)
         if(flag==1):
-            bins[binSeleccionado].append(j)
+            bins[binSeleccionado][0].append(j)
+            bins[binSeleccionado][1]= min(bins[binSeleccionado][1], partes[j][6])
+            bins[binSeleccionado][2]= encontrartiCama(bins[binSeleccionado], partes)
             #print("Se agrega el parte", j, "al bin", binSeleccionado)
             #print(bins)
         elif(flag==0):
             name="B"+str(len(bins)+1)
-            bins[name]=[j]
+            bins[name]=[[j], partes[j][6], partes[j][7]]
             ocupacion = ocupadoBin(bins[name], partes)
             #print("Se crea el bin", name, "y su ocupacion es", ocupacion)
-            #print(bins)
-    return bins
-              
+    return bins      
 def main():
     global partes
     quemado=int(input("Desea que se ejecute el sistema por defecto?\n0 No\n1 Si\n"))
@@ -94,10 +92,10 @@ def main():
         partes["J3"]=[1, 1, 1, 1, 1, 1, 6, 3, 0.5]
         partes["J4"]=[1, 1, 1, 1, 1, 1, 1, 1, 0.2]
         partes["J5"]=[1, 1, 1, 1, 1, 1, 1, 1, 0.4]
-        partes["J6"]=[1, 1, 1, 1, 1, 1, 4, 6, 0.2]
+        partes["J6"]=[1, 1, 1, 1, 1, 1, 8, 6, 0.2]
         partes["J7"]=[1, 1, 1, 1, 1, 1, 5, 3, 0.5]
         partes["J8"]=[1, 1, 1, 1, 1, 1, 7, 2, 0.1]
-        partes["J9"]=[1, 1, 1, 1, 1, 1, 3, 4, 0.6]
+        partes["J9"]=[1, 1, 1, 1, 1, 1, 7, 4, 0.6]
     elif(quemado==0):
         cantidadPartes=int(input("Ingrese la cantidad de partes a imprimir\n"))
         partes=dict()
@@ -112,6 +110,9 @@ def main():
             volumenParte= calcularVolumen(areaParte, alturaParte)
             plazoEntregaParte=float(input("Ingrese el plazo de entrega de la parte " + nomParte + "\n"))
             tiempoEjecucionParte=float(input("Ingrese el tiempo de ejecucion de la parte " + nomParte + " (Recuerde que este valor lo da CURA)\n"))
+            if(tiempoEjecucionParte>=plazoEntregaParte):
+                print("No se puede ingresar una parte que su tiempo de ejecucion sea mayor al plazo de entrega")
+                return
             weightParte=float(input("Ingrese el peso de la parte " + nomParte + " (Este valor se obtiene mientras el proyecto se toma unidimensional)\n"))
             parte=[largoParte, anchoParte, alturaParte, tipoParte, areaParte, volumenParte, plazoEntregaParte, tiempoEjecucionParte, weightParte]
             partes[nomParte] = parte
@@ -119,9 +120,7 @@ def main():
     sizeBin=1
     bins=dict()
     partes2=sorted(partes.keys(), key = lambda x: partes[x][8], reverse=True)
-    print(partes2)
     bins = bestFit(sizeBin, bins,partes,partes2)
-    print(bins)
-    
-    
+    camasOrdenadas=agendamientoCamas(bins, 7)
+    print(camasOrdenadas)
 main()
